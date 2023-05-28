655002100fa2e4c19e4589c851c3caee973f3c39
Aderoju Adeyemi <64233556+rojuadeyemi@users.noreply.github.com>
2023-05-28 11:07:33 +0100

    Update greeting.py

--- weather_app/greeting.py
+++ weather_app/greeting.py
@@ -14,7 +14,7 @@ def greet(ip_address):
     temp_data = get_temperature(location_data['lat'],
                                 location_data['lon'],
                                 location_data['timezone'])
-    temp_C = temp_data.iloc[0][0]
+    temp_C = temp_data.iloc[0]
     temp_F = convert_to_fahr(temp_C)
     wiki_query = wikipedia.search(f"""{location_data['city']},
                                   {location_data['country']}""")[0]
@@ -50,9 +50,7 @@ def get_temperature(lat, lon, timezone):
     times = [entry['time'] for entry in data]
     temps = [entry['data']['instant']['details']['air_temperature']
              for entry in data]
-    humid = [entry['data']['instant']['details']['relative_humidity']
-             for entry in data]
-    temp_data = pd.DataFrame({'temp':temps,'humid': humid}, index=times)
+    temp_data = pd.Series(temps, index=times)
     temp_data.index = (pd.to_datetime(temp_data.index)
                          .tz_convert(timezone))
     return temp_data
@@ -62,17 +60,17 @@ def plot_forecast(data):
     """
     Plot line graph of 24hr forecast, and bar graph of 10-day max & min.
     """
-    
+    temp24 = data[:24]
     temp24H_graph = StringIO()
-    fig = px.line_3d(data.iloc[1:25],y='temp', x=data.iloc[1:25].index,z='humid',
+    fig = px.line(y=temp24, x=temp24.index,
                   title="24 Hour Forecast", width=1200, height=900)
     
-    fig.update_xaxes(title_text='Time')
-    fig.update_yaxes(title_text='Temperature (C)')
+    fig.update_xaxes(title_text='Date')
+    fig.update_yaxes(title_text='Air temperature in deg C')
     fig.write_html(temp24H_graph, include_plotlyjs='cdn',
                    full_html=False)
 
-    temp10D = data['temp'].resample('1D').agg(['max', 'min'])
+    temp10D = data.resample('1D').agg(['max', 'min'])
     temp10D_graph = StringIO()
     fig2 = px.bar(temp10D,
                   barmode='group', title="10 Day Forecast", width=1200,
