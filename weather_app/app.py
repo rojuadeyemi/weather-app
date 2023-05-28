259e6e06f7b1efaba95f86d243de0fa9ee3c97dd
Aderoju Adeyemi <64233556+rojuadeyemi@users.noreply.github.com>
2023-05-28 05:56:45 +0100

    Update app.py

--- weather_app/app.py
+++ weather_app/app.py
@@ -3,18 +3,12 @@ from flask import Flask, request, render_template
 from weather_app.greeting import greet, get_local_IP_address
 
 app = Flask(__name__)
-DEPLOY = os.environ.get('DEPLOY')
-
 
 @app.route('/')
 def main():
-    if DEPLOY == 'railway':
-        ip_address = request.headers['X-Forwarded-For']
-    else:
-        ip_address = get_local_IP_address()
+    ip_address = get_local_IP_address()
 
     return render_template('index.html', weather_info=greet(ip_address))
-
 
 if __name__ == '__main__':
     app.run()
